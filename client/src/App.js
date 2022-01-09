2c66b1b501a539e15292981cced3e84650759c11
duduki <pdwtop0105@naver.com>
2022-01-09 21:50:01 +0900

    build : create Login feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div className="App">kakao talk</div>} />
+        <Route
+          path="/"
+          element={isLogin ? <div>로그인 성공</div> : <Login />}
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/Login.js
@@ -0,0 +1,28 @@
+import react from "react";
+import kakao from "../images/Kakao.png";
+import "../css/Login.css";
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-inner-container">
+        <div className="login-inner-container-header">
+          <img src={kakao} />
+        </div>
+        <div className="login-inner-container-body">
+          <input className="login-data-input"></input>
+          <input className="login-data-input"></input>
+          <div className="login-button">로그인</div>
+          <div className="auto-login">
+            <input type="checkbox" />
+            <div>실행시 자동 로그인</div>
+          </div>
+        </div>
+        <div className="login-inner-container-footer">
+          <div>카카오계정 찾기</div>
+          <div>비밀번호 재설정</div>
+        </div>
+      </div>
+    </div>
+  );
+}
