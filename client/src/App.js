a9835d52b6935979ded466c5ce2dfc23405414ed
duduki <pdwtop0105@naver.com>
2022-01-09 23:38:53 +0900

    fix : remove react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
-import FriendPage from "./pages/FriendPage";
 import MainPage from "./pages/MainPage";
 
 function App() {
   const [isLogin, setIsLogin] = useState(true);
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* 로그인이 되어있지 않은경우, 첫 화면이 로그인페이지가 된다. */}
-        {/* 로그인이 되어있는 경우, 바로 메인페이지로 넘긴다. */}
-        <Route path="/" element={isLogin ? <MainPage /> : <Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <>{isLogin ? <MainPage /> : <Login />}</>;
 }
 
 export default App;
