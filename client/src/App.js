6b74529a49fac49e4d4e4188153961897acaa3e9
duduki <pdwtop0105@naver.com>
2022-01-09 23:34:27 +0900

    build : create Nav feature and css

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
+import FriendPage from "./pages/FriendPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={isLogin ? <div>로그인 성공</div> : <Login />}
-        />
+        {/* 로그인이 되어있지 않은경우, 첫 화면이 로그인페이지가 된다. */}
+        {/* 로그인이 되어있는 경우, 바로 메인페이지로 넘긴다. */}
+        <Route path="/" element={isLogin ? <MainPage /> : <Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/FriendPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../css/pages/FriendPage.css";
+
+export default function FriendPage() {
+  return <div className="friendpage-container"></div>;
+}
