4ccb343a3a37d9f8405680719c4feeb25014548d
duduki <pdwtop0105@naver.com>
2022-01-10 23:04:45 +0900

    build : create seemorePage Nav feature

---
+++ client/src/components/seemore/SeeMoreNav.js
@@ -0,0 +1,9 @@
+import "../../css/components/seemore/SeeMoreNav.css";
+
+export default function SeeMoreNav(params) {
+  return (
+    <div className="seemore-page-nav">
+      <div className="seemore-page-nav-left">더보기</div>
+    </div>
+  );
+}
--- client/src/pages/SeeMorePage.js
+++ client/src/pages/SeeMorePage.js
@@ -1,3 +1,15 @@
+import SeeMoreNav from "../components/seemore/SeeMoreNav";
+import '../css/pages/SeeMorePage.css'
+
 export default function SeeMorePage() {
-  return <div>더보기!</div>;
+  return (
+    <div className="seemore-page-container">
+      <SeeMoreNav />
+      <div className="seemore-page-content">
+        <div className="seemore-page-content-header"></div>
+        <div className="seemore-page-content-body"></div>
+        <div className="seemore-page-content-footer"></div>
+      </div>
+    </div>
+  );
 }
