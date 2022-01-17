57740153a762cd8e2b8d0f3c29cff480fd222114
duduki <pdwtop0105@naver.com>
2022-01-17 22:28:02 +0900

    fix : findFriend feature

--- server/controllers/friends/findFriend.js
+++ server/controllers/friends/findFriend.js
@@ -1,29 +1,52 @@
 require("dotenv").config();
-// const { User, Friend } = require("../../models");
+const db = require("../../database/");
 
 module.exports = async function findFriend(req, res) {
+  // 로그인한 유저의 정보를 가져온다.
   const { id } = req.userInfo;
   // 친구 이메일
   const { email } = req.body;
-  // 이메일 정보로 로그인한 유저의 정보를 가져온다.
   // 주어진 이메일로 데이터를 확인해서 그 사람의 아이디를 찾는다.
   try {
-    const friendInfo = await User.findOne({ where: { email } });
-    // 아이디가 유효하지 않을 경우
-    if (!friendInfo) {
-      return res.status(404).send({ message: "null" });
-    }
-    const friend_id = friendInfo.dataValues.id;
-    delete friendInfo.dataValues.password;
-    const checkExistaFriend = await Friend.findOne({
-      where: { user_id: id, friend_id },
+    db.query(`select * from USERS where email="${email}"`, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      const friendInfo = result[0];
+      // 아이디가 존재하지 않을 경우
+      if (!friendInfo) {
+        return res.status(404).send();
+      }
+
+      // 친구정보 획득
+      const friend_id = friendInfo.id;
+
+      // 나 스스로를 검색하면 에러
+      if (friend_id === id) {
+        return res.status(404).send();
+      }
+      delete friendInfo.password;
+
+      // 이미 있는 친구인지 확인한다.
+      db.query(
+        `select * from friends where user_id='${id}' and friend_id='${friend_id}'`,
+        (err, result2) => {
+          if (err) {
+            throw err;
+          }
+          const data = result2[0];
+          // 친구 정보가 있다면,
+          if (data) {
+            return res
+              .status(202)
+              .send({ friendInfo, message: "aleady exist" });
+          }
+          // 친구 정보가 없다면
+          return res.status(201).send({ friendInfo, message: "ok" });
+        }
+      );
     });
-    // 이미 친구 정보가 존재하는 경우
-    if (checkExistaFriend) {
-      return res.status(202).send({ friendInfo, message: "aleady exist" });
-    }
-    // 존재하지 않는경우, 그 사람 데이터만 보내준다 -> 추가는 나중에
-    return res.status(201).send({ friendInfo, message: "ok" });
   } catch {
     return res.status(500).send({ message: "server error" });
   }
