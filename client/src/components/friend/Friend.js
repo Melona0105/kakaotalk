2c204e23791184d64fb48469b74b0e49155ffbce
duduki <pdwtop0105@naver.com>
2022-01-18 03:08:45 +0900

    build : create friend chatting feature

--- client/src/components/friend/Friend.js
+++ client/src/components/friend/Friend.js
@@ -1,21 +1,45 @@
 import react from "react";
+import axios from "axios";
 import user1 from "../../images/friend/user1.png";
+import Popup from "../etc/Popup";
+import { useEffect, useState } from "react";
 import "../../css/components/friend/Friend.css";
 import Melon from "../../components/etc/Melon";
 
 export default function Friend({ id, src, name, comment, music }) {
-  const user_id = id;
+  const [isChattingOn, setIsChattingOn] = useState(false);
+  const [currentRoomId, setCurrentRommId] = useState(undefined);
+  const friend_id = id;
 
   // 더블클릭하면, 현재 유저 아이디의 방과 + 클릭한 유저 아이디의 방중 같은 방이 있는지 찾는다.
   // 방이 있다면 그 방으로 연결
 
   // 방이 없다면, 새로 만들어서 서로 아이디에 그 방을 연결해주고 연결
+  const roomStyle = "top=100, left=100, width=375, height=640";
 
+  async function getRoomData() {
+    try {
+      const { data } = await axios({
+        method: "POST",
+        url: "http://localhost:4000/rooms",
+        headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
+        data: { friend_id },
+      })
+        .then((res) => res)
+        .catch((err) => console.log(err));
+      setCurrentRommId(data.room_id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <div
       className="friend-container"
-      onDoubleClick={() => console.log(user_id)}
+      onDoubleClick={() => {
+        getRoomData();
+        setIsChattingOn(true);
+      }}
     >
       <div className="friend-profile">
         {src ? <img src={src} /> : <img src={user1} />}
@@ -25,6 +49,14 @@ export default function Friend({ id, src, name, comment, music }) {
         </div>
       </div>
       {music && <Melon music={music} />}
+      {isChattingOn && (
+        <Popup
+          style={roomStyle}
+          url={`/room/${friend_id}`}
+          username={name}
+          callback={setIsChattingOn}
+        ></Popup>
+      )}
     </div>
   );
 }
--- server/controllers/rooms/getRoomInfo.js
+++ server/controllers/rooms/getRoomInfo.js
@@ -1,20 +1,67 @@
-// const { User, Friend, Room } = require("../../models");
+const db = require("../../database/");
 
 module.exports = async function getRoomInfo(req, res) {
   // 더블클릭하면, 현재 유저 아이디의 방과 + 클릭한 유저 아이디의 방중 같은 방이 있는지 찾는다.
-  // 방이 있다면 그 방으로 연결
   const user_id = req.userInfo.id;
   const { friend_id } = req.body;
 
+  console.log(user_id, friend_id);
   try {
     // 주어진 아이디에 해당하는 방들을 모두 가져온다.
-    console.log(user_id, friend_id);
-    const userRoomdata = await User.getProducts({});
+    // 로그인한 사람의 방과 주어진 아이디에 해당하는 사람의 방중 같은 아이디를 찾는다.
+    db.query(
+      `select * from users_in_rooms as A INNER JOIN users_in_rooms as B where A.user_id='${user_id}' AND B.user_id=${friend_id} AND A.room_id = B.room_id`,
+      (err, result1) => {
+        if (err) {
+          throw err;
+        }
+        const userRoom = result1[0];
 
-    console.log(userRoomdata);
+        // 방이 있다면 그 방으로 연결
+        if (userRoom) {
+          return res
+            .status(201)
+            .send({ room_id: userRoom.room_id, message: "ok" });
+        }
 
-    return res.status(200).send({ message: "hi!" });
-    // 방이 없다면, 새로 만들어서 서로 아이디에 그 방을 연결해주고 연결
+        // 방이 없다면, 새로 만들어서 서로 아이디에 그 방을 연결해주고 연결
+        db.query(`INSERT into rooms (noti) VALUES (0)`, (err, result2) => {
+          if (err) {
+            throw err;
+          }
+          // 새 방 생성됨
+
+          // 새 방의 id값 어떻게 주워 올것?
+          // 추가하고, 현재의 id를 다른 방에 추가한다.
+          db.query(`select id from rooms`, (err, result3) => {
+            if (err) {
+              throw err;
+            }
+
+            const room_Id = result3[result3.length - 1];
+
+            db.query(
+              `INSERT into users_in_rooms (user_id, room_id) VALUES (${user_id}, ${room_Id.id})`,
+              (err, result4) => {
+                if (err) {
+                  throw err;
+                }
+
+                db.query(
+                  `INSERT into users_in_rooms (user_id, room_id) VALUES (${friend_id}, ${room_Id.id})`,
+                  (err, result5) => {
+                    if (err) {
+                      throw err;
+                    }
+                    return res.status(201).send({ room_Id });
+                  }
+                );
+              }
+            );
+          });
+        });
+      }
+    );
   } catch {
     return res.status(500).send({ message: "server error" });
   }
