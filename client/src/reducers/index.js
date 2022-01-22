0d4ac61a89c8dc481108ba896e6a2ec2e07a84bd
duduki <pdwtop0105@naver.com>
2022-01-22 11:24:53 +0900

    fix : redux-persist for using sessionStorage

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -9,11 +9,12 @@ import MsgChangeReducer from "./chattingRoom/MsgChangeReducer";
 import UserFriendsInfoReducer from "./mainPage/UserFriendsInfoReducer";
 
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage"; // 이거는 로컬스토리지 -> 탭 닫아도 직접 수정하기 전까지는 지속됨
+import storageSession from "redux-persist/lib/storage/session"; // 이거는 세션스토리지 -> 탭 닫으면 초기화
 
 const persistConfig = {
   key: "root,",
-  storage,
+  storage:storageSession,
 };
 
 const appReducer = combineReducers({
