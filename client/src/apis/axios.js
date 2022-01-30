8196220cb219bea3d17e9624000709ce43fe3dc7
duduki <pdwtop0105@naver.com>
2022-01-30 17:54:18 +0900

    fix: aixos instance

--- client/src/apis/axios.js
+++ client/src/apis/axios.js
@@ -2,9 +2,6 @@ import axios from "axios";
 import { server } from "../utils";
 
 const axiosInstance = axios.create({ baseURL: server });
-// const axiosInstance = axios.create({
-//   baseURL: "http://ec2-3-36-51-139.ap-northeast-2.compute.amazonaws.com:8080",
-// });
 
 axiosInstance.interceptors.request.use((config) => {
   config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
