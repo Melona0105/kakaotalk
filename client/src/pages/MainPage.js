c86afc58b3b1ee0f706909a8dcfe622be0d5cac9
duduki <pdwtop0105@naver.com>
2022-01-30 19:52:33 +0900

    fix : fix chatting room bugs

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -40,7 +40,7 @@ export default function MainPage() {
     return () => {
       client.off("message");
     };
-  }, []);
+  });
 
   // 들어온 데이터 안의 배열들을 순회하면서 거기서 일치하는 값을 뽑아낸다.
   function getNewMessage(array) {
